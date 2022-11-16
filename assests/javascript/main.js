5b203f7ed55801db27f90f7a86a634d40fbc85da
Chere Lemma <chere.lemma@aastu.edu.et>
2022-11-16 11:46:29 +0300

    Implement highlight top nav menus up on page scroll

--- assests/javascript/main.js
+++ assests/javascript/main.js
@@ -14,3 +14,23 @@ NavLinks.forEach((linkItem) => {
     topMenu.classList.remove('mobile-top-nav');
   });
 });
+
+/* Highlight top nav menus up on page scroll */
+const sections = document.querySelectorAll("section[id]");
+
+window.addEventListener("scroll", function () {
+  let scrollY = window.pageYOffset;
+  
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = (current.getBoundingClientRect().top + window.pageYOffset) - 300;
+    sectionId = current.getAttribute("id");
+    
+    if (scrollY > sectionTop &&  scrollY <= sectionTop + sectionHeight){
+      document.querySelector(".top-nav-menu a[href*=" + sectionId + "]").classList.add("highlight");
+    } 
+    else {
+      document.querySelector(".top-nav-menu a[href*=" + sectionId + "]").classList.remove("highlight");
+    }
+  });
+});
