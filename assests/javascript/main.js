b982d22e013c9531a7f3f122770d19cd30e28d3b
raihan2bd <raihan2bd.official@gmail.com>
2022-11-16 13:53:40 +0600

    Driver 2 Added The Navigation Item event in main.js

--- assests/javascript/main.js
+++ assests/javascript/main.js
@@ -4,7 +4,13 @@ const topMenu = document.querySelector('.top-nav-menu');
 const NavLinks = document.querySelectorAll('.navlinks');
 
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    topMenu.classList.toggle('mobile-top-nav');
+  hamburger.classList.toggle('active');
+  topMenu.classList.toggle('mobile-top-nav');
+});
+
+NavLinks.forEach((linkItem) => {
+  linkItem.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    topMenu.classList.remove('mobile-top-nav');
   });
-  
+});
